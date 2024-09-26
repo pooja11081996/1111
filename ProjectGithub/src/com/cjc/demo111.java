70e82ff45c10c0ddb4e9bef37b7022b639280e34
aaradhyan <Aaradhya.Nachankar@tapasi.com>
2024-09-26 11:05:19 +0530

    Its me

---
+++ ProjectGithub/src/com/cjc/demo111.java
@@ -0,0 +1,17 @@
+package com.cjc;
+
+public class demo111 {
+public void m1()
+{
+	System.out.println("m1 method");
+}
+public void m2()
+{
+	System.out.println("m2 method");
+}
+public static void main(String[] args) {
+	{
+		System.out.println("Hello");
+	}
+}
+}
